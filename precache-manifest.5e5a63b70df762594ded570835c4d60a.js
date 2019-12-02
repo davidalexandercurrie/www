90552fdebecc432c06255f0768a1c4bf212cd13f
zozo <47287113+zozo-url@users.noreply.github.com>
2019-12-02 17:07:54 +1300

    Update precache-manifest.5e5a63b70df762594ded570835c4d60a.js

--- precache-manifest.5e5a63b70df762594ded570835c4d60a.js
+++ precache-manifest.5e5a63b70df762594ded570835c4d60a.js
@@ -5,14 +5,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "f9f283f5e873a8953a7e",
-    "url": "/www/static/js/2.7715b69d.chunk.js"
+    "url": "/static/js/2.7715b69d.chunk.js"
   },
   {
     "revision": "4f8410f7619ed6dd248c",
-    "url": "/www/static/js/main.89725cf3.chunk.js"
+    "url": "/static/js/main.89725cf3.chunk.js"
   },
   {
     "revision": "de5e90447687860003db",
-    "url": "/www/static/js/runtime-main.7339b8b4.js"
+    "url": "/static/js/runtime-main.7339b8b4.js"
   }
 ]);
